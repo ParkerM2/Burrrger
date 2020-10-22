05f30b2e5a6ab21a5d30cb9fd0a29d1258180c1b
Parker <parkerlmanning@hotmail.com>
2020-10-22 18:02:28 -0500

    added controller page folder

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,40 @@
+var express = require('express');
+const orm = require('../config/orm');
+var burger = require('../models/burger');
+var router = express.Router();
+// homepage
+router.get('/', function (req,res) {
+    res.redirect('/index')
+});
+
+// Creating routes and logic
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var object = {
+            burgers : data
+        };
+        console.log(object)
+        res.render("index", object)
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    orm.insertOne(["burger_name"],[req.body.burger_name],function (result) {
+        // might need to change result data here
+        res.json({id : result.insertId})
+    }); 
+});
+
+router.put("/api/burgers/:id", function (req,res) {
+    console.log(req)
+    burger.updateOne({devoured: req.body.devoured},
+        "id =" + req.params.id, function(result) {
+            if (result.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+});
+
+module.exports = router;
