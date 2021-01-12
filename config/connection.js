bbbae906f249e42644b29df2da156c5f9ddff752
Parker <parkerlmanning@hotmail.com>
2021-01-12 17:11:05 -0600

    jaws db settings

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,21 @@
+const mysql = require('mysql');
+var connection;
 
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+if(process.env.JAWSDB_URL)
+ {
+     connection = mysql.createConnection(process.env.JAWSDB_URL)
+ } else {
+     require('dotenv').config();
+     connection = mysql.createConnection({
+         host: process.env.DB_HOST,
+         user: process.env.DB_USERNAME,
+         password: process.env.DB_PASSWORD,
+         database: process.env.DB_NAME
+     });
 
-
-
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "ManningSigns1",
-//     database: "burgers_db"
-// })
-
-connection.connect(function (err) {
-    if (err) {
-    console.error("error connection " , err.stack);
-    return;
-}
-console.log("connected as id", connection.threadId)
-})
-
+ }
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Database connected!");
+});
 module.exports = connection;
